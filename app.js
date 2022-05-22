90853f1cfe001d38e116d1e72ff990274fc6e5f2
Skrewed <annnddr@gmail.com>
2022-05-22 16:51:50 -0300

    Extracting Day, Month and Year from Date Input

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+var selectedDate, day, month, year
+
 //Listen to btn-calc click and returns the calculation
 document.querySelector(".btn-calc").addEventListener('click', function (){
-    alert('I was clicked!')
+    if (day == undefined) {
+        alert('Selecione uma data.')
+    } else {
+        alert(`Day: ${day}\nMonth: ${month}\nYear: ${year}`);
+    }
+})
+
+//Listen to input date change
+document.querySelector(".date").addEventListener('change', function(){
+    day = this.value.split("-")[2]
+    month = this.value.split("-")[1]
+    year = this.value.split("-")[0]
 })
